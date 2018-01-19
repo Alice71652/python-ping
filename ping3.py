b3317b7f53fd1c9b5df4fadcfa564dedaa2acfa1
Alice <496203111@qq.com>
2018-01-19 18:43:08 +0900

    support for python 3

--- ping3.py
+++ ping3.py
@@ -2,65 +2,48 @@
 
 """
     A pure python ping implementation using raw socket.
-
-
     Note that ICMP messages can only be sent from processes running as root.
-
-
     Derived from ping.c distributed in Linux's netkit. That code is
     copyright (c) 1989 by The Regents of the University of California.
     That code is in turn derived from code written by Mike Muuss of the
     US Army Ballistic Research Laboratory in December, 1983 and
     placed in the public domain. They have my thanks.
-
     Bugs are naturally mine. I'd be glad to hear about them. There are
     certainly word - size dependenceies here.
-
     Copyright (c) Matthew Dixon Cowles, <http://www.visi.com/~mdc/>.
     Distributable under the terms of the GNU General Public License
     version 2. Provided with no warranties of any sort.
-
     Original Version from Matthew Dixon Cowles:
       -> ftp://ftp.visi.com/users/mdc/ping.py
-
     Rewrite by Jens Diemer:
       -> http://www.python-forum.de/post-69122.html#69122
-
-
     Revision history
     ~~~~~~~~~~~~~~~~
-
     March 11, 2010
     changes by Samuel Stauffer:
     - replaced time.clock with default_timer which is set to
       time.clock on windows and time.time on other systems.
-
     May 30, 2007
     little rewrite by Jens Diemer:
      -  change socket asterisk import to a normal import
      -  replace time.time() with time.clock()
      -  delete "return None" (or change to "return" only)
      -  in checksum() rename "str" to "source_string"
-
     November 22, 1997
     Initial hack. Doesn't do much, but rather than try to guess
     what features I (or others) will want in the future, I've only
     put in what I need now.
-
     December 16, 1997
     For some reason, the checksum bytes are in the wrong order when
     this is run under Solaris 2.X for SPARC but it works right under
     Linux x86. Since I don't know just what's wrong, I'll swap the
     bytes always and then do an htons().
-
     December 4, 2000
     Changed the struct.pack() calls to pack the checksum and ID as
     unsigned. My thanks to Jerome Poincheval for the fix.
-
     Januari 27, 2015
     Changed receive response to not accept ICMP request messages.
     It was possible to receive the very request that was sent.
-
     Last commit info:
     ~~~~~~~~~~~~~~~~~
     $LastChangedDate: $
@@ -91,7 +74,7 @@ def checksum(source_string):
     countTo = (len(source_string)/2)*2
     count = 0
     while count<countTo:
-        thisVal = ord(source_string[count + 1])*256 + ord(source_string[count])
+        thisVal = ord(chr(source_string[count + 1]))*256 + ord(chr(source_string[count]))
         sum = sum + thisVal
         sum = sum & 0xffffffff # Necessary?
         count = count + 2
@@ -104,10 +87,8 @@ def checksum(source_string):
     sum = sum + (sum >> 16)
     answer = ~sum
     answer = answer & 0xffff
-
     # Swap bytes. Bugger me if I know why.
     answer = answer >> 8 | (answer << 8 & 0xff00)
-
     return answer
 
 
@@ -136,12 +117,11 @@ def receive_one_ping(my_socket, ID, timeout):
             bytesInDouble = struct.calcsize("d")
             timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
             return timeReceived - timeSent
-
+        
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
             return
-
-
+        
 def send_one_ping(my_socket, dest_addr, ID):
     """
     Send one ping to the given >dest_addr<.
@@ -155,7 +135,7 @@ def send_one_ping(my_socket, dest_addr, ID):
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
     bytesInDouble = struct.calcsize("d")
     data = (192 - bytesInDouble) * "Q"
-    data = struct.pack("d", default_timer()) + data
+    data = struct.pack("d", default_timer()) + data.encode('ASCII')
 
     # Calculate the checksum on the data and the dummy header.
     my_checksum = checksum(header + data)
@@ -166,8 +146,7 @@ def send_one_ping(my_socket, dest_addr, ID):
         "bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1
     )
     packet = header + data
-    my_socket.sendto(packet, (dest_addr, 1)) # Don't know about the 1  (The 1 means port/protocol number. The protocol number of icmp is 1)
-
+    my_socket.sendto(packet, (dest_addr, 1)) # Don't know about the 1(The 1 means port/protocol number. The protocol number of icmp is 1)
 
 def do_one(dest_addr, timeout):
     """
@@ -176,14 +155,12 @@ def do_one(dest_addr, timeout):
     icmp = socket.getprotobyname("icmp")
     try:
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-    except socket.error, (errno, msg):
-        if errno == 1:
-            # Operation not permitted
-            msg = msg + (
-                " - Note that ICMP messages can only be sent from processes"
-                " running as root."
-            )
-            raise socket.error(msg)
+    except socket.error as e:
+        msg = e + (
+            " - Note that ICMP messages can only be sent from processes"
+            " running as root."
+        )
+        raise socket.error(msg)
         raise # raise the original error
 
     my_ID = os.getpid() & 0xFFFF
@@ -194,30 +171,34 @@ def do_one(dest_addr, timeout):
     my_socket.close()
     return delay
 
-
 def verbose_ping(dest_addr, timeout = 2, count = 4):
     """
     Send >count< ping to >dest_addr< with the given >timeout< and display
     the result.
     """
-    for i in xrange(count):
-        print "ping %s..." % dest_addr,
+    for i in range(count):
+        print ("ping %s..." % dest_addr)
         try:
             delay  =  do_one(dest_addr, timeout)
-        except socket.gaierror, e:
-            print "failed. (socket error: '%s')" % e[1]
+        except socket.gaierror as e:
+            print ("failed. (socket error: '%s')" % e)
             break
-
+        except OSError as e:
+            print ("failed. '%s'" % e)
+            break
         if delay  ==  None:
-            print "failed. (timeout within %ssec.)" % timeout
+            print ("failed. (timeout within %ssec.)" % timeout)
         else:
             delay  =  delay * 1000
-            print "get ping in %0.4fms" % delay
-    print
+            print ("get ping in %0.4fms" % delay)
 
 
 if __name__ == '__main__':
+
+    verbose_ping("123")
+    verbose_ping("abc.dfwe")
     verbose_ping("heise.de")
     verbose_ping("google.com")
     verbose_ping("a-test-url-taht-is-not-available.com")
     verbose_ping("192.168.1.1")
+
